c8faf2e2e38b0e22b050f0b91c164d4eaf54f2fb
Beowulf <62406369+Five-Kobolds@users.noreply.github.com>
2022-05-12 10:43:33 +0100

    Commented on Menu.cs

--- Computer Science Game/Assets/Scripts/HUDs & Functionality/GUI/Main_Menu.cs
+++ Computer Science Game/Assets/Scripts/HUDs & Functionality/GUI/Main_Menu.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// ^ Default unity functionality requirements + Scene Tools.
 
 public class Main_Menu : MonoBehaviour
 {
+
+    // When called, set the active scene
+    // to 1 (Level 1)
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    // When called, Exit the game.
     public void QuitGame()
     {
         Application.Quit();
