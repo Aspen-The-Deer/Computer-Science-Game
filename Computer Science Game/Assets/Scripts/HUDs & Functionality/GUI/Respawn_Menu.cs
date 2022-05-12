1bc10f692137929580a213b0282350dd2128fe8e
Beowulf <62406369+Five-Kobolds@users.noreply.github.com>
2022-05-12 10:48:26 +0100

    Commented on Respawn.cs

--- Computer Science Game/Assets/Scripts/HUDs & Functionality/GUI/Respawn_Menu.cs
+++ Computer Science Game/Assets/Scripts/HUDs & Functionality/GUI/Respawn_Menu.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// ^ Default unity functionality requirements + Scene Tools.
 
 public class Respawn_Menu : MonoBehaviour
 {
+
+    // When first run, Unlock the cursor so that
+    // it can be used to use the menu.
     public void Awake()
     {
         Cursor.lockState = CursorLockMode.None;
     }
+    
+    // When called, set the active scene
+    // to 4 (Main Menu)
     public void MainMenu()
     {
         SceneManager.LoadScene(4);
     }
 
+    // When called, Exit the game.
     public void QuitGame()
     {
         Application.Quit();
