3cdccbc3230b45b07d418eaa5da16b24bbef1043
Beowulf <62406369+Five-Kobolds@users.noreply.github.com>
2022-05-12 10:37:05 +0100

    Commented Level.cs

--- Computer Science Game/Assets/Scripts/HUDs & Functionality/GUI/Level_Select.cs
+++ Computer Science Game/Assets/Scripts/HUDs & Functionality/GUI/Level_Select.cs
@@ -2,25 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// ^ Default unity functionality requirements + Scene Tools.
 
 public class Level_Select : MonoBehaviour
 {
 
+    // When called, set the active scene
+    // to 0 (Level 0)
     public void Level0()
     {
         SceneManager.LoadScene(0);
     }
 
+    // When called, set the active scene
+    // to 1 (Level 1)
     public void Level1()
     {
         SceneManager.LoadScene(1);
     }
 
+    // When called, set the active scene
+    // to 2 (Level 2)
     public void Level2()
     {
         SceneManager.LoadScene(2);
     }
 
+    // When called, set the active scene
+    // to 3 (Level 3)
     public void Level3()
     {
         SceneManager.LoadScene(3);
